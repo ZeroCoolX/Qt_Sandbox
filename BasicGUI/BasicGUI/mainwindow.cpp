7121682fb24e2b1f12bb43ca7421e5dfffde4172
cnpatton <botanicality@gmail.com>
2019-06-20 20:58:47 -0500

    BasicGUI: Adding basic gui

---
+++ BasicGUI/BasicGUI/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+// Button slot
+void MainWindow::on_pushButtonClose_clicked()
+{
+    ui->label->setText("Button is clicked!");
+}
